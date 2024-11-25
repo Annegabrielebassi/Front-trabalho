4e86808dcc14747332b6018ccbce5b0283d1caa0
Annegabrielebassi <a.bassi@estudante.ifro.edu.br>
2024-11-25 19:26:17 -0400

    editei

--- TrabalhoFront]/src/app/page.js
+++ TrabalhoFront]/src/app/page.js
@@ -6,14 +6,9 @@ export default function Home() {
     <div>
       <header />
       <h1>
-        Bem-vindo ao meu mundo de girassóis!
+        Anne Gabrielly
       </h1>
-      <p>
-        Em campos dourados, girassóis a dançar,
-        Sob o sol radiante, começam a brilhar.
-        Com pétalas abertas, seguem a luz,
-        Em um balé de cores, a vida seduz.
-      </p>
+      <p className={styles.Titulo}>Girassois são legais</p>
     </div>
   );
 }
--- TrabalhoFront]/src/app/state/page.js
+++ TrabalhoFront]/src/app/state/page.js
@@ -1,31 +1,10 @@
 'use client'
-import { useState } from "react"
 import styles from "./state.module.css"
 
 export default function State() {
-    const [name, setName] = useState('');
-    const [message, setMessage] = useState('');
-
-    const handleInputChange = (e) => {
-        setName(e.target.value);
-    };
-
-    const handleButtonClick = () => {
-        setMessage(`Olá ${name}, Hello Kitty te dá as boas-vindas!`);
-    };
-
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>Hello Kitty</h1>
-            <input 
-                className={styles.input} 
-                type="text" 
-                value={name} 
-                onChange={handleInputChange} 
-                placeholder="Digite seu nome" 
-            />
-            <button className={styles.button} onClick={handleButtonClick}>Enviar</button>
-            {message && <p className={styles.message}>{message}</p>}
+            <h1 className={styles.title}>Anne Gabrielly</h1>
         </div>
     )
 }
